4c824945e807b6f5652f7ee81c1cc28b307faa9f
blin04 <ivanbokor17@gmail.com>
2022-07-18 12:31:08 +0200

    deleted unnecessary files that were hidden (now all of the unnecessary files are deleted)

--- kernelModule/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/Module.symvers := sed 's/\.ko$$/\.o/' /home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/modules.order | scripts/mod/modpost -m -a  -o /home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/Module.symvers -e -i Module.symvers   -T -
--- kernelModule/.tasks.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/tasks.ko := ld -r  -m elf_x86_64  --build-id=sha1  -T scripts/module.lds -o /home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/tasks.ko /home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/tasks.o /home/ivan/Documents/Petnica/Project2022/Modules/tasksModule/tasks.mod.o;  true
