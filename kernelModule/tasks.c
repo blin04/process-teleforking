ec23f22aefef5b105820072f3c05e848a3b5b916
blin04 <ivanbokor17@gmail.com>
2022-08-16 18:12:23 +0200

    module prints paths to files that process has opened (testing for changing the kernel code)

--- kernelModule/tasks.c
+++ kernelModule/tasks.c
@@ -6,13 +6,14 @@
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/sched.h>
-#include <linux/sched/task.h>
+#include <linux/fdtable.h>
+#include <linux/dcache.h>
+#include <linux/gfp.h>
 #include <linux/perf_event.h> 
 #include <linux/delay.h> 
 #include "commands.h"
 
 #define MAJOR_NUM	100
-#define MAX_LENGTH	255
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -29,18 +30,27 @@ static int tasks_close(struct inode *device_file, struct file *instance) {
 static long int tasks_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	// defining variables used in switch cases
-	struct kernel_clone_args kargs;
+	//struct kernel_siginfo info;
 	struct task_struct *task; 
-	struct kernel_siginfo info;
+	struct files_struct *current_files;
+	struct fdtable *files_table;
+	struct path files_path;
+	unsigned int *fds;
 	pid_t process_pid;
 	int ret;
 	int fnd = 0;
+	int i = 0;
+
+	// variable for printing file path
+	char *cwd; 	
+	// buffer used by d_path
+	char *buff = (char *)kmalloc(100 * sizeof(char), GFP_KERNEL);
 
 	switch(cmd) {
 		case IOCTL_CLN:
 			/*
-			 * Find process with the given PID
-			 * and clone it
+			 * Print paths of files which the task, that has the 
+			 * given PID, has opened
 			 */
 
 			if (copy_from_user(&process_pid, (pid_t *)arg, sizeof(process_pid))) {
@@ -61,40 +71,30 @@ static long int tasks_ioctl(struct file *file, unsigned int cmd, unsigned long a
 				
 				// check if process was found;
 				if (fnd) {
-					/* stop found process and restart it later */
+					/* print all open files of a process */
+					printk(KERN_INFO "TasksModule: Task %d uses following files\n", task->pid);
 
-/*					memset(&info, 0, sizeof(struct kernel_siginfo));
+					current_files = task->files;
+					files_table = files_fdtable(current_files);
+
+					while(files_table->fd[i] != NULL) {
+						files_path = files_table->fd[i]->f_path;
+						// make string of path
+						cwd = d_path(&files_path, buff, 100*sizeof(char));
+
+						printk(KERN_INFO "%d, %s", i, cwd);
+						++i;
+					}
+
+/*			this chuck of code is going to be useful later		
+					memset(&info, 0, sizeof(struct kernel_siginfo));
 					info.si_signo = SIGSTOP;
 					ret = send_sig_info(SIGSTOP, &info, task);
 					if (ret < 0) {
 						printk(KERN_INFO "error sending signal\n");
 					}
 					printk(KERN_INFO "TasksModule: sent stop signal\n");
-					task->__state = __TASK_STOPPED;
-					printk(KERN_INFO "TasksModule: Process state is %d\n", task->__state); */
-
-
-					kargs.exit_signal = SIGCHLD;
-					kargs.parent_tid = &(task->pid);
-
-					kernel_clone(&kargs);
-
-/*					ret = wake_up_process(task);
-					if (ret == 0) {
-						printk(KERN_INFO "TasksModule: process was already running\n");
-					}
-					else printk(KERN_INFO "TasksModule: process woken up\n");
-
-					msleep(8000);
-
-					memset(&info, 0, sizeof(struct kernel_siginfo));
-					info.si_signo = SIGCONT;
-					ret = send_sig_info(SIGCONT, &info, task);
-					if (ret < 0) {
-						printk(KERN_INFO "error sending signal\n");
-					}
-					printk(KERN_INFO "TasksModule: sent cont signal\n"); */
-
+*/
 					return 0;
 				}
 			} 
