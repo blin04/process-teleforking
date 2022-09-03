798e2ec2dc0ca3de017b3250c6a26ed88ea1fe3c
blin04 <ivanbokor17@gmail.com>
2022-09-03 21:45:15 +0200

    merged scripts for starting qemu into one script

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+sudo losetup -fP ../disk.img
+sudo mount /dev/loop26 ~/Documents/Petnica/Project2022/Code/Shared/
+
 sudo qemu-system-x86_64 \
 	-kernel /home/ivan/Documents/Petnica/Project2022/KernelSourceFiles/obj/linux-86-basic/arch/x86_64/boot/bzImage \
     -initrd /home/ivan/Documents/Petnica/Project2022/KernelSourceFiles/initramfs3.cpio.gz \
-	-hdb /dev/loop27 \
+	-hdb /dev/loop26 \
     -nographic -append "console=ttyS0"
