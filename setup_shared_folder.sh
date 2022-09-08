db626417ed1f616a3ae7401cebe169ae9468be50
blin04 <ivanbokor17@gmail.com>
2022-09-08 18:01:43 +0200

    split script for starting qemu in to two parts (first in which the shared folder is setup and second in which the qemu starts

---
+++ setup_shared_folder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo losetup -fP ../disk.img
+sudo mount /dev/loop26 ~/Documents/Petnica/Project2022/Code/Shared/
+
--- start_qemu.sh
+++ start_qemu.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-sudo losetup -fP ../disk.img
-sudo mount /dev/loop26 ~/Documents/Petnica/Project2022/Code/Shared/
-
 sudo qemu-system-x86_64 \
 	-kernel /home/ivan/Documents/Petnica/Project2022/KernelSourceFiles/obj/linux-86-basic/arch/x86_64/boot/bzImage \
-    -initrd /home/ivan/Documents/Petnica/Project2022/KernelSourceFiles/initramfs3.cpio.gz \
+    -initrd /home/ivan/Documents/Petnica/Project2022/KernelSourceFiles/minimal-initramfs.cpio.gz \
 	-hdb /dev/loop26 \
     -nographic -append "console=ttyS0"
