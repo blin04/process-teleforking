6fb5adab529da0baea6cafa652812e832fb111cb
blin04 <ivanbokor17@gmail.com>
2022-09-11 12:18:39 +0200

    module now prints open files of a process which has the given PID

--- kernelModule/tasks.c
+++ kernelModule/tasks.c
@@ -6,6 +6,8 @@
 #include <linux/sched.h>
 #include <linux/file.h>
 #include <linux/fs.h>
+#include <linux/pid.h>
+#include <linux/sched.h>
 #include <linux/dcache.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
@@ -14,6 +16,17 @@
 #define MAJOR_NUM	100
 
 MODULE_LICENSE("Dual BSD/GPL");
+
+static struct task_struct *find_task(pid_t pid)
+{
+	struct task_struct *task;
+	for_each_process(task) {
+		if (task->pid == pid) {
+			return task;
+		} 
+	}
+	return task;
+}
 
 static int tasks_open(struct inode *device_file, struct file *instance) {
 	printk(KERN_INFO "TasksModule: opened device file!\n");
@@ -31,20 +44,37 @@ static long int tasks_ioctl(struct file *file, unsigned int cmd, unsigned long a
 	struct task_struct *task;
 	struct files_struct *current_files;
 	struct fdtable *files_table;
+	struct path files_path;
 	int i = 0;
 	pid_t process_pid;
+
 	char *final_path;
 	char *buff = (char *)kmalloc(GFP_KERNEL, 100*sizeof(char));
 
 	switch(cmd) {
 		case IOCTL_CLN:
 			if (copy_from_user(&process_pid, (pid_t *)arg, sizeof(process_pid))) {
-				printk(KERN_ALERT "TasksModule: can continue writing this code\n");
-			}
-			else {
 				printk(KERN_ALERT "TasksModule: Couldn't copy PID from the user!\n");
+				return -1;
 			}
 
+			printk(KERN_INFO "TasksModule: Copied PID sucessfuly, %d\n", process_pid);
+			printk(KERN_INFO "TasksModule: Open files are... \n");
+
+			
+			task = find_task(process_pid);
+			
+			current_files = task->files;
+			files_table = files_fdtable(current_files);
+			while(files_table->fd[i] != NULL) {
+				files_path = files_table->fd[i]->f_path;
+				final_path = d_path(files_path.dentry, files_path.mnt, buff, 100*sizeof(char));
+
+				printk(KERN_INFO "**** %s *****\n", final_path);
+				++i;
+			}
+			
+			return 0;
 		default:
 			return -1;
 	}
@@ -71,7 +101,7 @@ static int hello_init(void)
 		printk(KERN_ALERT "TasksModule: Failed to register the device. Change the major number!\n");
 		return -1;
 	}
-	printk(KERN_INFO "TasksModule: Sucessful registration, Major Number is: %d", number);
+	printk(KERN_INFO "TasksModule: Sucessful registration\n");
 
 	return 0;
 }
